91f3d3fbc5738daa9a4c036901459a19328025f1
joaocandidozup <joao.candido@zup.com.br>
2024-12-02 16:22:16 -0300

    feat:Add variable declaration exercise

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "João";
+        int idade = 25;
+        double altura = 1.75;
+        boolean estaMatriculaNoCurso = true;
+
+        System.out.println("Nome:"+nome + " Idade:"+ idade + " Altura:" + altura + " Matriculado:" + estaMatriculaNoCurso);
+
+
+    }
+}
